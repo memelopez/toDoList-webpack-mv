00df093023c488217bbdeabedfd6f934d561ce0c
memelopez <meme30lopez@gmail.com>
2022-01-12 17:29:35 -0600

    Remove hardcodded tasks

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import Task from './modules/task';
 import UI from './modules/ui';
 import validateDescription from './modules/helpfulFunctions';
 
-const task1 = new Task('wash dishes', 0);
-const task2 = new Task('clean room', 1);
-
-const tasks = [];
-tasks.push(task1);
-tasks.push(task2);
-
 // When content loads
 document.addEventListener('DOMContentLoaded', UI.displayTasks());
 
