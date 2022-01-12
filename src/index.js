f8513faa41a40010501893e77c30af387f685c19
memelopez <meme30lopez@gmail.com>
2022-01-12 11:29:17 -0600

    Add hardcodded tasks and function that on content load

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 // index.js
 import './styles.css';
+import Task from './modules/task';
+import UI from './modules/ui';
 
-function component() {
+const task1 = new Task('wash dishes', 0);
+const task2 = new Task('clean room', 1);
 
+const tasks = [];
+tasks.push(task1);
+tasks.push(task2);
 
-}
-
-document.body.appendChild(component());
+// When content loads
+document.addEventListener('DOMContentLoaded', UI.displayTasks(tasks));
