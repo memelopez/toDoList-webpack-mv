9cf76870989a328c90196020ed05dc582e3ed7f9
memelopez <meme30lopez@gmail.com>
2022-01-12 14:39:03 -0600

    Add event listener for form submit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import './styles.css';
 import Task from './modules/task';
 import UI from './modules/ui';
+import { validateDescription } from './modules/helpfulFunctions';
 
 const task1 = new Task('wash dishes', 0);
 const task2 = new Task('clean room', 1);
@@ -12,3 +13,16 @@ tasks.push(task2);
 
 // When content loads
 document.addEventListener('DOMContentLoaded', UI.displayTasks());
+
+// Event: when form is submitted
+document.querySelector('#addTask').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get form values
+  const taskDescription = document.querySelector('#taskDesc').value.trim();
+  if (validateDescription(taskDescription)) {
+    // Add task
+    UI.addTask(taskDescription);
+  }
+  document.querySelector('#taskDesc').value = '';
+});
