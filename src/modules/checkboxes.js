c72460dd72e04117d5dd72dd65fbdca9fa156c15
memelopez <meme30lopez@gmail.com>
2022-01-12 20:39:05 -0600

    Fix linters

--- src/modules/checkboxes.js
+++ src/modules/checkboxes.js
@@ -1,13 +1,13 @@
 // checkboxes.js
 
-import Store from "./store";
+import Store from './store';
 
 // function to change isCompleted value of tasks
 const taskCompleted = (position2chage, value) => {
   // Gets list from local storage
-  let toDos = Store.getTasks();
+  const toDos = Store.getTasks();
 
-  let task2modify = toDos[position2chage];
+  const task2modify = toDos[position2chage];
   task2modify.isCompleted = value;
   toDos[position2chage] = task2modify;
 
