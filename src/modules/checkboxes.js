227f9810cb7fd7d979bf3dede0b6d5708dad131f
memelopez <meme30lopez@gmail.com>
2022-01-24 21:45:59 -0600

    Change function so that it doesn't call displayTasks()

--- src/modules/checkboxes.js
+++ src/modules/checkboxes.js
@@ -3,7 +3,7 @@
 import Store from './store';
 
 // function to change isCompleted value of tasks
-const taskCompleted = (position2chage, value) => {
+const taskCompleted = (position2chage, value, pSib) => {
   // Gets list from local storage
   const toDos = Store.getTasks();
 
@@ -11,8 +11,18 @@ const taskCompleted = (position2chage, value) => {
   task2modify.isCompleted = value;
   toDos[position2chage] = task2modify;
 
+
   // Set items to storage
   Store.setTasks(toDos);
+
+  // change strike through class
+  if (value) {
+    pSib.classList.add('text-decoration-line-through');
+  } else {
+    let pClasses = pSib.className;
+    pClasses = pClasses.replace('text-decoration-line-through', '');
+    pSib.className = pClasses;
+  }
 };
 
 export default taskCompleted;
